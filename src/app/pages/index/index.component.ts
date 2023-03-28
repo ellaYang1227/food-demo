87ef34ac52ea346442d38c974a4607f3c38a7606
ella <yanzi19841227@gmail.com>
2023-03-28 23:02:37 +0800

    新增產品

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ProductService } from '@services/product.service';
-import { forkJoin } from 'rxjs';
+import { debounceTime, distinctUntilChanged, forkJoin, map } from 'rxjs';
 
 @Component({
   selector: 'app-index',
@@ -8,11 +10,18 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  search = {
+    category: '',
+    keyword: '',
+  };
   currentCategory: string = '';
   categorys!: Array<any>;
   products!: Array<any>;
+  filterProducts!: Array<any>;
+  @ViewChild('searchInput') searchInput!: NgModel;
 
   constructor(
+    private domSanitizer: DomSanitizer,
     private productService: ProductService
   ) { }
 
@@ -23,12 +32,32 @@ export class IndexComponent implements OnInit {
     ]).subscribe((resArr: Array<any>) => {
       console.log(resArr)
       this.categorys = resArr[0];
-      this.currentCategory = this.categorys[0].name;
+      this.search.category = this.categorys[0].name;
       this.products = resArr[1];
+      this.getFilterProducts();
     });
+
+    this.searchInput?.valueChanges?.pipe(
+      debounceTime(500),
+      map((val: string) => val.trim()),
+      distinctUntilChanged()
+    )
+      .subscribe(res => {
+        console.log(res)
+      })
+  }
+
+  getFilterProducts() {
+    const { category, keyword } = this.search;
+    this.filterProducts = this.products.filter((product: any) => product.category === category && (!keyword || product.name === keyword));
   }
 
   switchCategory(category: string) {
-    this.currentCategory = category;
+    this.search.category = category;
+    this.getFilterProducts();
+  }
+
+  getSafeImageUrl(imgUrl: string): SafeUrl {
+    return this.domSanitizer.bypassSecurityTrustUrl(imgUrl)
   }
 }
